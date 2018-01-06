245119ea5a9713ef22376501fb3f778f24f03894
Thomas (Junrui) Hu <junruihu@gmail.com>
2018-01-06 11:17:32 -0500

    feature: double click to select only that operator

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+function rand_int(a, b){
+    return Math.floor(Math.random() * (b - a + 1)) + a
+}
+
+function rand_elem(xs){
+    if (xs instanceof Set){
+        xs = Array.from(xs)
+    }
+    return xs[rand_int(0, xs.length - 1)]
+}
+
+function time_format_seconds(){
+    return performance.now() / 1000
+}
+
+function play_sound_effect(id){
+    // makes sure that sound effect plays from start even when it's still playing
+    aud = document.getElementById(id)
+    aud.pause()
+    aud.currentTime = 0
+    aud.play()
+}
+
 new Vue({
     el: "#app",
     data: {
@@ -10,6 +33,7 @@ new Vue({
 
         op: null,
         ops: {0:'+', 1:'-', 2:'*', 3:'/'},
+        // ops: new Set("+-*/"),
         op_symbols: {'+':'+', '-':'−', '*':'⋅', '/':'/'},
         ops_used: [true, true, true, true],
         active_ops: [0, 1, 2, 3],
@@ -34,51 +58,43 @@ new Vue({
             this.score = 0;
             this.game_started = true;
             this.new_problem();
-            this.start_time = this.time_format();
+            this.start_time = time_format_seconds();
         },
         new_problem(){
             this.n_smallest = Math.pow(10, this.low_selected - 1);
             this.n_largest = Math.pow(10, this.high_selected) - 1;
 
-            this.first_num = this.rand_int(this.n_smallest, this.n_largest);
-            this.second_num = this.rand_int(this.n_smallest, this.n_largest);
-            // gen from 1 to lcm(2, 3, 4) = 12
+            this.first_num = rand_int(this.n_smallest, this.n_largest);
+            this.second_num = rand_int(this.n_smallest, this.n_largest);
 
-            /*
-            if (!this.ops_used.includes(true)){
-                location.reload();
-            } */
-            this.op = this.ops[this.active_ops[this.rand_int(0, 11) % Object.keys(this.active_ops).length]];
+            this.op = this.ops[rand_elem(this.active_ops)];
             if (this.op == '/'){
-                // special case for division
+                // special case for division, treat as multiplication in reverse
                 [this.first_num, this.answer] = [this.first_num * this.second_num, this.first_num]
             } else {
                 this.answer = eval(this.first_num + ' ' + this.op + ' ' + this.second_num);
             }
             this.response = "";
         },
-        rand_int(a, b){
-            return Math.floor(Math.random() * (b - a + 1)) + a;
-        },
-        time_format(){
-            return performance.now() / 1000;
-        },
-        play_sound_effect(id){
-            // makes sure that sound effect plays from start even when it's still playing
-            aud = document.getElementById(id);
-            aud.pause();
-            aud.currentTime = 0;
-            aud.play();
-        },
         toggle_op(op_id){
-            this.play_sound_effect("audio_buttons");
-            this.ops_used[op_id] = !this.ops_used[op_id];
-            if (!this.ops_used.includes(true)){
-                alert("must use at least one operator!");
-                //todo: vue.js may have better way
-                document.getElementById(this.button_names[op_id]).click();
+            play_sound_effect("audio_buttons");
+            if (op_id < 0){
+                op_id = -op_id - 1
+                for (let i = 0; i < 4; i ++){
+                    if ((i === op_id) !== this.ops_used[i]){
+                        document.getElementById(this.button_names[i]).click();
+                    }
+                }
+            } else {
+                this.ops_used[op_id] = !this.ops_used[op_id];
+                if (!this.ops_used.includes(true)){
+                    // alert("must use at least one operator!");
+                    //todo: vue.js may have better way
+                    document.getElementById(this.button_names[op_id]).click();
+                }
+                this.active_ops = Object.keys(this.ops).filter(x => this.ops_used[x]);
+                console.log(this.active_ops)
             }
-            this.active_ops = Object.keys(this.ops).filter(x => this.ops_used[x]);
         },
     },
     computed: {
@@ -87,7 +103,7 @@ new Vue({
             if (isCorrect){
 
                 // Add to times
-                now = this.time_format();
+                now = time_format_seconds();
                 // after adding, floats become inaccurate again, hence need to use toFixed
                 this.response_times.push(parseFloat((now - this.start_time).toFixed(2)));
                 this.start_time = now;
@@ -95,11 +111,11 @@ new Vue({
                 this.score ++;
 
                 if (this.score >= this.target_score){
-                    this.play_sound_effect("audio_applause");
+                    play_sound_effect("audio_applause");
                     alert("Congrats, you've completed your daily practice!");
                     this.game_started = false;
                 } else {
-                    this.play_sound_effect("audio_success");
+                    play_sound_effect("audio_success");
                     this.new_problem();
                 }
             } else if (this.response === 's'){
